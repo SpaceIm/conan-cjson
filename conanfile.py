1984d8483b2b90292be9529999fc82ebd3d2f27a
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-04-03 00:56:44 +0200

    install dll in proper dir through patch

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,4 @@
-import glob
 import os
-import shutil
 
 from conans import ConanFile, CMake, tools
 
@@ -11,7 +9,7 @@ class CjsonConan(ConanFile):
     topics = ("conan", "cjson", "json", "parser")
     homepage = "https://github.com/DaveGamble/cJSON"
     url = "https://github.com/conan-io/conan-center-index"
-    exports_sources = "CMakeLists.txt"
+    exports_sources = ["CMakeLists.txt", "patches/**"]
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {
@@ -48,6 +46,9 @@ class CjsonConan(ConanFile):
         os.rename("cJSON-" + self.version, self._source_subfolder)
 
     def build(self):
+        if "patches" in self.conan_data and self.version in self.conan_data["patches"]:
+            for patch in self.conan_data["patches"][self.version]:
+                tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -78,15 +79,6 @@ class CjsonConan(ConanFile):
         cmake.install()
         tools.rmdir(os.path.join(self.package_folder, "lib", "cmake"))
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
-        self._move_dll_to_bin_folder()
-
-    def _move_dll_to_bin_folder(self):
-        if self.settings.os == "Windows" and self.options.shared:
-            bin_dir = os.path.join(self.package_folder, "bin")
-            if not os.path.exists(bin_dir):
-                os.mkdir(bin_dir)
-            for dll_file in glob.glob(os.path.join(self.package_folder, "lib", "*.dll")):
-                shutil.move(dll_file, bin_dir)
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
