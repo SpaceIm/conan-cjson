407c3904a738e5a7e6a6bc1e823c076533c54c03
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-11 21:26:46 +0200

    add pkg_config name

--- conanfile.py
+++ conanfile.py
@@ -81,6 +81,7 @@ class CjsonConan(ConanFile):
         tools.rmdir(os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
+        self.cpp_info.names["pkg_config"] = "libcjson"
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
             self.cpp_info.system_libs.append("m")
