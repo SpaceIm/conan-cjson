9ba212d3e5cc6cddbe0ea1f040bfc8696b254923
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2020-05-11 21:27:54 +0200

    more elegant loop over patches

--- conanfile.py
+++ conanfile.py
@@ -46,9 +46,8 @@ class CjsonConan(ConanFile):
         os.rename("cJSON-" + self.version, self._source_subfolder)
 
     def build(self):
-        if "patches" in self.conan_data and self.version in self.conan_data["patches"]:
-            for patch in self.conan_data["patches"][self.version]:
-                tools.patch(**patch)
+        for patch in self.conan_data.get("patches", {}).get(self.version, []):
+            tools.patch(**patch)
         cmake = self._configure_cmake()
         cmake.build()
 
